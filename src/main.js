52b2a337b4bb52e1b9ec65a89f1599ca6f6c7450
Bhagam Chandan Chowdary <chandanchoudary@hotmail.com>
2019-07-03 21:44:57 +0530

    Intial commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+// importing Vue and related
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+
+// importing bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home row">
+    <div class="menu col-xl-4 col-md-6">
+      <div class="menu-wrapper">
+        <div class="menu-option"
+          v-for="(option, index) in options"
+          :key="index">
+            {{option}}
+        </div>
+      </div>
+    </div>
+    <video class="background-video" autoplay loop>
+      <source src="@/assets/hero.webm" type="video/webm">
+    </video>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      options: [
+        'Anime',
+        'Movies',
+        'Music',
+        'Games',
+        'Projects',
+        'Things',
+        'About',
+        'And Stuff',
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.background-video {
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: -100;
+}
+
+video {
+  object-fit: cover;
+}
+
+.menu {
+  color: white;
+  height: 100%;
+  position:absolute;
+  padding:0px;
+  background: linear-gradient(to right, rgba(0,0,0,0.6), rgba(0,0,0,0));
+}
+
+.menu-wrapper {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 100%;
+}
+
+.home {
+  margin: 0px;
+}
+
+.menu-option {
+  margin: 30px;
+  font-size: 1.5rem;
+  font-family: 'B612', sans-serif;
+}
+</style>
